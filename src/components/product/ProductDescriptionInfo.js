48bcacadc67c0bc91a518f2ca7be767982bb017c
prandeveloper <87694761+prandeveloper@users.noreply.github.com>
2021-12-30 19:11:42 +0530

    search

--- src/components/product/ProductDescriptionInfo.js
+++ src/components/product/ProductDescriptionInfo.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { getProductCartQuantity } from "../../helpers/product";
+//import { getProductCartQuantity } from "../../helpers/product";
 import { addToCart } from "../../redux/actions/cartActions";
 import { addToWishlist } from "../../redux/actions/wishlistActions";
 import { addToCompare } from "../../redux/actions/compareActions";
@@ -21,65 +21,70 @@ const ProductDescriptionInfo = ({
   addToCart,
   addToWishlist,
   addToCompare,
+  fullProductDesc,
 }) => {
-  const [selectedProductColor, setSelectedProductColor] = useState(
-    product.variation ? product.variation[0].color : ""
-  );
-  const [selectedProductSize, setSelectedProductSize] = useState(
-    product.variation ? product.variation[0].size[0].name : ""
-  );
-  const [productStock, setProductStock] = useState(
-    product.variation ? product.variation[0].size[0].stock : product.stock
-  );
+  // const [selectedProductColor, setSelectedProductColor] = useState(
+  //   product.variation ? product.variation[0].color : ""
+  // );
+  // const [selectedProductSize, setSelectedProductSize] = useState(
+  //   product.variation ? product.variation[0].size[0].name : ""
+  // );
+  // const [productStock, setProductStock] = useState(
+  //   product.variation ? product.variation[0].size[0].stock : product.stock
+  // );
   const [quantityCount, setQuantityCount] = useState(1);
 
-  const productCartQty = getProductCartQuantity(
-    cartItems,
-    product,
-    selectedProductColor,
-    selectedProductSize
-  );
+  // const productCartQty = getProductCartQuantity(
+  //   cartItems,
+  //   product,
+  //   selectedProductColor,
+  //   selectedProductSize
+  // );
+  const [state, setstate] = React.useState({});
+  //const [imgArr, setImgArr] = React.useState([]);
+  React.useEffect(() => {
+    console.log("Full Desc", fullProductDesc);
+    // console.log("Product Img", productImage);
+    // var t = JSON.parse(productImage).product_img;
+    // if (t !== undefined && t !== null) setImgArr(t);
+    // setstate(JSON.parse(productImage));
+    if (
+      fullProductDesc !== undefined &&
+      fullProductDesc !== null &&
+      fullProductDesc !== ""
+    )
+      setstate(JSON.parse(fullProductDesc));
+  });
 
   return (
     <div className="product-details-content ml-70">
-      <h2>{product.name}</h2>
+      <h2>{state?.product_name}</h2>
       <div className="product-details-price">
-        {discountedPrice !== null ? (
-          <Fragment>
-            <span>{currency.currencySymbol + finalDiscountedPrice}</span>{" "}
-            <span className="old">
-              {currency.currencySymbol + finalProductPrice}
-            </span>
-          </Fragment>
-        ) : (
-          <span>{currency.currencySymbol + finalProductPrice} </span>
-        )}
+        <Fragment>
+          <span>{state?.sell_price}</span>{" "}
+        </Fragment>
       </div>
-      {product.rating && product.rating > 0 ? (
-        <div className="pro-details-rating-wrap">
+
+      {/* <div className="pro-details-rating-wrap">
           <div className="pro-details-rating">
             <Rating ratingValue={product.rating} />
           </div>
-        </div>
-      ) : (
-        ""
-      )}
+        </div> */}
+
       <div className="pro-details-list">
-        <p>{product.shortDescription}</p>
+        <p>{state?.short_desc}</p>
       </div>
 
-      {product.variation ? (
-        <div className="pro-details-size-color">
-          <div className="pro-details-color-wrap">
-            <span>Color</span>
-            <div className="pro-details-color-content">
-              {product.variation.map((single, key) => {
-                return (
-                  <label
+      <div className="pro-details-size-color">
+        <div className="pro-details-color-wrap">
+          <span>Color</span>
+          <div className="pro-details-color-content">
+            <h5>{state?.color}</h5>
+            {/* <label
                     className={`pro-details-color-content--single ${single.color}`}
-                    key={key}
-                  >
-                    <input
+                   
+                  > */}
+            {/* <input
                       type="radio"
                       value={single.color}
                       name="product-color"
@@ -92,16 +97,15 @@ const ProductDescriptionInfo = ({
                         setProductStock(single.size[0].stock);
                         setQuantityCount(1);
                       }}
-                    />
-                    <span className="checkmark"></span>
-                  </label>
-                );
-              })}
-            </div>
+                    /> */}
+            {/* <span className="checkmark"></span>
+                  </label> */}
           </div>
-          <div className="pro-details-size">
-            <span>Size</span>
-            <div className="pro-details-size-content">
+        </div>
+        <div className="pro-details-size">
+          <span>Size</span>
+          <h6>{state?.size}</h6>
+          {/* <div className="pro-details-size-content">
               {product.variation &&
                 product.variation.map(single => {
                   return single.color === selectedProductColor
@@ -131,176 +135,64 @@ const ProductDescriptionInfo = ({
                       })
                     : "";
                 })}
-            </div>
-          </div>
+            </div> */}
         </div>
-      ) : (
-        ""
-      )}
-      {product.affiliateLink ? (
-        <div className="pro-details-quality">
-          <div className="pro-details-cart btn-hover ml-0">
-            <a
-              href={product.affiliateLink}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Buy Now
-            </a>
-          </div>
-        </div>
-      ) : (
-        <div className="pro-details-quality">
-          <div className="cart-plus-minus">
-            <button
-              onClick={() =>
-                setQuantityCount(quantityCount > 1 ? quantityCount - 1 : 1)
-              }
-              className="dec qtybutton"
-            >
-              -
-            </button>
-            <input
-              className="cart-plus-minus-box"
-              type="text"
-              value={quantityCount}
-              readOnly
-            />
-            <button
-              onClick={() =>
-                setQuantityCount(
-                  quantityCount < productStock - productCartQty
-                    ? quantityCount + 1
-                    : quantityCount
-                )
-              }
-              className="inc qtybutton"
-            >
-              +
-            </button>
-          </div>
-          <div className="pro-details-cart btn-hover">
-            {productStock && productStock > 0 ? (
-              <button
-                onClick={() =>
-                  addToCart(
-                    product,
-                    addToast,
-                    quantityCount,
-                    selectedProductColor,
-                    selectedProductSize
-                  )
-                }
-                disabled={productCartQty >= productStock}
-              >
-                {" "}
-                Add To Cart{" "}
-              </button>
-            ) : (
-              <button disabled>Out of Stock</button>
-            )}
-          </div>
-          <div className="pro-details-wishlist">
-            <button
-              className={wishlistItem !== undefined ? "active" : ""}
-              disabled={wishlistItem !== undefined}
-              title={
-                wishlistItem !== undefined
-                  ? "Added to wishlist"
-                  : "Add to wishlist"
-              }
-              onClick={() => addToWishlist(product, addToast)}
-            >
-              <i className="pe-7s-like" />
-            </button>
-          </div>
-          <div className="pro-details-compare">
-            <button
-              className={compareItem !== undefined ? "active" : ""}
-              disabled={compareItem !== undefined}
-              title={
-                compareItem !== undefined
-                  ? "Added to compare"
-                  : "Add to compare"
-              }
-              onClick={() => addToCompare(product, addToast)}
-            >
-              <i className="pe-7s-shuffle" />
-            </button>
-          </div>
-        </div>
-      )}
-      {product.category ? (
-        <div className="pro-details-meta">
-          <span>Categories :</span>
-          <ul>
-            {product.category.map((single, key) => {
-              return (
-                <li key={key}>
-                  <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
-                    {single}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      ) : (
-        ""
-      )}
-      {product.tag ? (
-        <div className="pro-details-meta">
-          <span>Tags :</span>
-          <ul>
-            {product.tag.map((single, key) => {
-              return (
-                <li key={key}>
-                  <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
-                    {single}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      ) : (
-        ""
-      )}
+      </div>
 
-      <div className="pro-details-social">
-        <ul>
-          <li>
-            <a href="//facebook.com">
-              <i className="fa fa-facebook" />
-            </a>
-          </li>
-          <li>
-            <a href="//dribbble.com">
-              <i className="fa fa-dribbble" />
-            </a>
-          </li>
-          <li>
-            <a href="//pinterest.com">
-              <i className="fa fa-pinterest-p" />
-            </a>
-          </li>
-          <li>
-            <a href="//twitter.com">
-              <i className="fa fa-twitter" />
-            </a>
-          </li>
-          <li>
-            <a href="//linkedin.com">
-              <i className="fa fa-linkedin" />
-            </a>
-          </li>
-        </ul>
+      <div className="pro-details-quality">
+        <div className="pro-details-cart btn-hover ml-0">
+          <a href="#" rel="noopener noreferrer" target="_blank">
+            Buy Now
+          </a>
+        </div>
+      </div>
+
+      <div className="pro-details-quality">
+        <div className="cart-plus-minus">
+          <button
+            onClick={() =>
+              setQuantityCount(quantityCount > 1 ? quantityCount - 1 : 1)
+            }
+            className="dec qtybutton"
+          >
+            -
+          </button>
+          <input
+            className="cart-plus-minus-box"
+            type="text"
+            value={quantityCount}
+            readOnly
+          />
+          <button className="inc qtybutton">+</button>
+        </div>
+        <div className="pro-details-cart btn-hover">
+          <button onClick={() => addToCart(product, addToast, quantityCount)}>
+            Add To Cart{" "}
+          </button>
+
+          <button disabled>Out of Stock</button>
+        </div>
+        <div className="pro-details-wishlist">
+          <button
+            className={wishlistItem !== undefined ? "active" : ""}
+            disabled={wishlistItem !== undefined}
+            title={
+              wishlistItem !== undefined
+                ? "Added to wishlist"
+                : "Add to wishlist"
+            }
+            onClick={() => addToWishlist(product, addToast)}
+          >
+            <i className="pe-7s-like" />
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 ProductDescriptionInfo.propTypes = {
+  fullProductDesc: PropTypes.string,
   addToCart: PropTypes.func,
   addToCompare: PropTypes.func,
   addToWishlist: PropTypes.func,
