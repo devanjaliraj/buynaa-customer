c9f4d9ac254c1988636fdb301c41549c96d8aa33
devanjaliraj <anjalidevelopersveltosest@gmail.com>
2022-01-02 22:35:10 +0530

    new

--- src/pages/shop/ShopGridTwoColumn.js
+++ src/pages/shop/ShopGridTwoColumn.js
@@ -4,14 +4,35 @@ import MetaTags from "react-meta-tags";
 import Paginator from "react-hooks-paginator";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getSortedProducts } from "../../helpers/product";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import ShopSidebar from "../../wrappers/product/ShopSidebar";
 import ShopTopbar from "../../wrappers/product/ShopTopbar";
-import ShopProducts from "../../wrappers/product/ShopProducts";
+//import ShopProducts from "../../wrappers/product/ShopProducts";
+//import ProductModal from "../../components/product/ProductModal";
+import { useToasts } from "react-toast-notifications";
 
-const ShopGridTwoColumn = ({ location, products }) => {
+const ShopGridTwoColumn = ({
+  location,
+  products,
+  product,
+  currency,
+  addToCart,
+  addToWishlist,
+  addToCompare,
+  cartItem,
+  wishlistItem,
+  discountedPrice,
+  compareItem,
+  sliderClassName,
+  spaceBottomClass,
+  colorClass,
+  titlePriceClass,
+}) => {
+  //const [modalShow, setModalShow] = useState(false);
+  const { addToast } = useToasts();
   const [layout, setLayout] = useState("grid two-column");
   const [sortType, setSortType] = useState("");
   const [sortValue, setSortValue] = useState("");
@@ -25,7 +46,7 @@ const ShopGridTwoColumn = ({ location, products }) => {
   const pageLimit = 16;
   const { pathname } = location;
 
-  const getLayout = layout => {
+  const getLayout = (layout) => {
     setLayout(layout);
   };
 
@@ -50,6 +71,21 @@ const ShopGridTwoColumn = ({ location, products }) => {
     setSortedProducts(sortedProducts);
     setCurrentData(sortedProducts.slice(offset, offset + pageLimit));
   }, [offset, products, sortType, sortValue, filterSortType, filterSortValue]);
+
+  const [dress, setDress] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("http://35.154.86.59/api/admin/getproduct");
+
+      const data = await response.json();
+
+      setDress(data.data);
+      console.log(data.data);
+    }
+    getData();
+    return;
+  }, []);
 
   return (
     <Fragment>
@@ -94,7 +130,201 @@ const ShopGridTwoColumn = ({ location, products }) => {
                 />
 
                 {/* shop page content default */}
-                <ShopProducts layout={layout} products={currentData} />
+                {/* <ShopProducts layout={layout} products={currentData} /> */}
+                <div className="shop-bottom-area mt-35">
+                  <div className={`row ${layout ? layout : ""}`}>
+                    {/* <ProductGridTwo
+                      products={products}
+                      spaceBottomClass="mb-25"
+                    /> */}
+                    <Fragment>
+                      {dress.map((product) => (
+                        <div
+                          className={`col-xl-3 col-md-6 col-lg-4 col-sm-6 ${
+                            sliderClassName ? sliderClassName : ""
+                          }`}
+                          key={product._id}
+                        >
+                          <div
+                            className={`product-wrap-2 ${
+                              spaceBottomClass ? spaceBottomClass : ""
+                            } ${colorClass ? colorClass : ""} `}
+                          >
+                            <div className="product-img">
+                              <Link
+                                to={
+                                  process.env.PUBLIC_URL +
+                                  "/product-sticky/" +
+                                  product._id
+                                }
+                              >
+                                <img
+                                  className="default-img"
+                                  src={product.product_img[0]}
+                                  alt=""
+                                  style={{ width: "200px", height: "250px" }}
+                                />
+
+                                <img
+                                  className="hover-img"
+                                  src={product.product_img[1]}
+                                  alt=""
+                                  style={{ width: "200px", height: "250px" }}
+                                />
+                              </Link>
+                              {/* {product.discount || product.new ? (
+                <div className="product-img-badges">
+                  {product.discount ? (
+                    <span className="pink">-{product.discount}</span>
+                  ) : (
+                    ""
+                  )}
+                  {product.new ? <span className="purple">New</span> : ""}
+                </div>
+              ) : (
+                ""
+              )} */}
+
+                              <div className="product-action-2">
+                                {product.affiliateLink ? (
+                                  <a
+                                    href={product.affiliateLink}
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    title="Buy now"
+                                  >
+                                    {" "}
+                                    <i className="fa fa-shopping-cart"></i>{" "}
+                                  </a>
+                                ) : product.variation &&
+                                  product.variation.length >= 1 ? (
+                                  <Link
+                                    to={`${process.env.PUBLIC_URL}/product/${product.id}`}
+                                    title="Select options"
+                                  >
+                                    <i className="fa fa-cog"></i>
+                                  </Link>
+                                ) : product.stock && product.stock > 0 ? (
+                                  <button
+                                    onClick={() => addToCart(product, addToast)}
+                                    className={
+                                      cartItem !== undefined &&
+                                      cartItem.quantity > 0
+                                        ? "active"
+                                        : ""
+                                    }
+                                    disabled={
+                                      cartItem !== undefined &&
+                                      cartItem.quantity > 0
+                                    }
+                                    title={
+                                      cartItem !== undefined
+                                        ? "Added to cart"
+                                        : "Add to cart"
+                                    }
+                                  >
+                                    {" "}
+                                    <i className="fa fa-shopping-cart"></i>{" "}
+                                  </button>
+                                ) : (
+                                  <button
+                                    disabled
+                                    className="active"
+                                    title="Out of stock"
+                                  >
+                                    <i className="fa fa-shopping-cart"></i>
+                                  </button>
+                                )}
+                                {/* 
+                                <button
+                                  onClick={() => setModalShow(true)}
+                                  title="Quick View"
+                                >
+                                  <i className="fa fa-eye"></i>
+                                </button> */}
+
+                                <button
+                                  className={
+                                    compareItem !== undefined ? "active" : ""
+                                  }
+                                  disabled={compareItem !== undefined}
+                                  title={
+                                    compareItem !== undefined
+                                      ? "Added to compare"
+                                      : "Add to compare"
+                                  }
+                                  onClick={() =>
+                                    addToCompare(product, addToast)
+                                  }
+                                >
+                                  <i className="fa fa-retweet"></i>
+                                </button>
+                              </div>
+                            </div>
+                            <div className="product-content-2">
+                              <div
+                                className={`title-price-wrap-2 ${
+                                  titlePriceClass ? titlePriceClass : ""
+                                }`}
+                              >
+                                <h3>
+                                  <Link
+                                    to={
+                                      process.env.PUBLIC_URL +
+                                      "/product/" +
+                                      product._id
+                                    }
+                                  >
+                                    {product.product_name}
+                                  </Link>
+                                </h3>
+                                <div className="price-2">
+                                  <Fragment>
+                                    <span>₹ {product.sell_price}</span>
+                                  </Fragment>
+                                </div>
+                              </div>
+                              <div className="pro-wishlist-2">
+                                <button
+                                  className={
+                                    wishlistItem !== undefined ? "active" : ""
+                                  }
+                                  disabled={wishlistItem !== undefined}
+                                  title={
+                                    wishlistItem !== undefined
+                                      ? "Added to wishlist"
+                                      : "Add to wishlist"
+                                  }
+                                  onClick={() =>
+                                    addToWishlist(product, addToast)
+                                  }
+                                >
+                                  <i className="fa fa-heart-o" />
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+
+                      {/* product modal */}
+                      {/* <ProductModal
+                        show={modalShow}
+                        onHide={() => setModalShow(false)}
+                        product={product}
+                        currency={currency}
+                        discountedprice={discountedPrice}
+                        cartitem={cartItem}
+                        wishlistitem={wishlistItem}
+                        compareitem={compareItem}
+                        addtocart={addToCart}
+                        addtowishlist={addToWishlist}
+                        addtocompare={addToCompare}
+                        addtoast={addToast}
+                      /> */}
+                    </Fragment>
+                  </div>
+                </div>
 
                 {/* shop product pagination */}
                 <div className="pro-pagination-style text-center mt-30">
@@ -124,7 +354,7 @@ ShopGridTwoColumn.propTypes = {
   products: PropTypes.array,
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     products: state.productData.products,
   };
